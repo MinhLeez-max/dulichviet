396995f38ac447bbd288bae0be7d7ff849ead001
MinhLeez-max <minh106025@donga.edu.vn>
2025-05-24 14:00:29 +0700

    Cập nhật toàn bộ code mới

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn <tournhanh>.wsgi:application
--- tourapp/settings.py
+++ tourapp/settings.py
@@ -4,6 +4,8 @@ Django settings for tourapp project.
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,11 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-abcdefghijklmnopqrstuvwxyz1234567890'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 # CSRF settings
@@ -71,10 +70,7 @@ WSGI_APPLICATION = 'tourapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
